867b9a597655b1fbb837be2dc2183f38b0e0e6a3
Adina - Diana Valcauan <adina.valcauan@gmail.com>
2023-03-13 13:47:11 +0200

    added video presentation

---
+++ code.ino/code.ino.ino
@@ -0,0 +1,45 @@
+#include <Servo.h>
+
+
+Servo myservo;  // create servo object to control a servo
+
+void setup() {
+  
+  Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(0);
+
+  pinMode(12,INPUT_PULLUP);
+  pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+  int now = 1;
+  static int currentState = 0;
+  static int onStreak = 0, offStreak = 0;
+  if(now == 1 && currentState == 0) {
+    offStreak = 0;
+    onStreak++;
+  }
+  if(now == 0 && currentState == 1) {
+    onStreak = 0;
+    offStreak++;
+  }
+
+  if(onStreak >= 500) {
+    onStreak = 0;
+    currentState = 1;
+    // rising edge -> switch UP
+    myservo.write(180);
+    digitalWrite(13,HIGH);
+    delay(1500);
+    digitalWrite(13,LOW);
+    myservo.write(0);
+    delay(1500);
+  } else if(offStreak >= 500) {
+    offStreak = 0;
+    currentState = 0;
+    // falling edge -> switch DOWN
+  }
+}
